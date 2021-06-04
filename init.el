76d62e03ed4203fbd4d5bbce1cc6f20476b0e093
Paw Møller <pawsen@gmail.com>
2021-06-04 13:09:33 +0200

    try the docker layer

--- init.el
+++ init.el
@@ -94,7 +94,7 @@
        ;; ansible
        (debugger +lsp)     ; FIXME stepping through code, to help you add bugs
        direnv              ; local env in .envrc file
-       ;;docker
+       (docker +lsp)
        editorconfig      ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
