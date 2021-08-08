b3b625d61e6fdf53116a92bb1777033f33f9d02c
Paw Møller <pawsen@gmail.com>
2021-08-08 09:42:47 +0200

    disable UI:treesitter for syntex highligth

--- init.el
+++ init.el
@@ -49,7 +49,7 @@
        ;;pretty-code       ; replace bits of code with pretty symbols
        ;;tabs              ; an tab bar for Emacs
        treemacs          ; a project drawer, like neotree but cooler
-       tree-sitter       ; syntax highligtning
+       ;; tree-sitter       ; syntax highligtning
        ;;unicode           ; extended unicode support for various languages
        vc-gutter         ; vcs diff in the fringe
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
